ce9b24daf70ccf6f0f6c01f5d44ca6f75b5682fe
Tch1b0 <jpour3006@gmail.com>
2022-10-22 16:24:03 +0200

    general: add `Number` type

--- pkg/general/general.go
+++ pkg/general/general.go
@@ -2,6 +2,10 @@ package general
 
 import "os"
 
+type Number interface {
+	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64
+}
+
 // read and process the input
 func ProcessInput[T any](path string, processor func(string) T) (T, error) {
 	file, err := os.Open(path)
