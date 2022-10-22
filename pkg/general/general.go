c67530546bf669326821b520b829737187a87dbd
Tch1b0 <jpour3006@gmail.com>
2022-10-22 15:56:59 +0200

    general: enhance `ProcessInput`

--- pkg/general/general.go
+++ pkg/general/general.go
@@ -2,10 +2,11 @@ package general
 
 import "os"
 
-func ReadInput(path string) (string, error) {
+// read and process the input
+func ProcessInput[T any](path string, processor func(string) T) (T, error) {
 	file, err := os.Open(path)
 	var content []byte
 	file.Read(content)
 
-	return string(content), err
+	return processor(string(content)), err
 }
