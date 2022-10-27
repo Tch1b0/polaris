17f804308eab0bf37d2ec54ccd4c33d3db374528
Tch1b0 <jpour3006@gmail.com>
2022-10-27 15:44:42 +0200

    input: add tests

---
+++ input/input_test.go
@@ -0,0 +1,44 @@
+package input_test
+
+import (
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/tch1b0/polaris/input"
+)
+
+func TestRead(t *testing.T) {
+	tmpFile, err := os.CreateTemp(os.TempDir(), "*")
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(tmpFile.Name())
+	tmpFile.WriteString("abc")
+
+	require.Equal(t, input.Read(tmpFile.Name()), "abc")
+}
+
+func TestProcessing(t *testing.T) {
+	tmpFile, err := os.CreateTemp(os.TempDir(), "*")
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(tmpFile.Name())
+	tmpFile.WriteString("1 2 3")
+
+	require.Equal(
+		t,
+		input.Process(tmpFile.Name(), func(in string) []int {
+			res := make([]int, 3)
+			for i, char := range strings.Split(in, " ") {
+				res[i], _ = strconv.Atoi(char)
+			}
+
+			return res
+		}),
+		[]int{1, 2, 3},
+	)
+}
