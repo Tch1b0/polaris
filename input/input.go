2c07381923d1e445de06ec89e7b0a61a9c80f0a5
Tch1b0 <jpour3006@gmail.com>
2022-11-08 15:27:08 +0100

    add memoize function

--- input/input.go
+++ input/input.go
@@ -22,3 +22,15 @@ func Read(path string) string {
 
 	return string(content)
 }
+
+func Memoize[T comparable, N any](fn func(T) N) func(T) N {
+	cache := make(map[T]N)
+	return func(in T) N {
+		if val, ok := cache[in]; ok {
+			return val
+		}
+
+		cache[in] = fn(in)
+		return cache[in]
+	}
+}
