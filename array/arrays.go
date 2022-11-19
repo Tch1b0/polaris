b9be631b2d326cfc5dff9899f3c81200c3df40e3
Tch1b0 <jpour3006@gmail.com>
2022-11-19 09:17:35 +0100

    arrays: add general array utility

--- array/arrays.go
+++ array/arrays.go
@@ -1,52 +1,61 @@
 package array
 
-func Map[T any, N any](arr []T, predicate func(T, int) N) []N {
-	newArr := make([]N, len(arr))
+// returns the position of value in arr or -1 if it does not exist
+func Index[T comparable](arr []T, value T) int {
 	for i, item := range arr {
-		newArr = append(newArr, predicate(item, i))
-	}
-
-	return newArr
-}
-
-func Filter[T any](arr []T, predicate func(T, int) bool) []T {
-	newArr := make([]T, len(arr))
-	for i, item := range arr {
-		if predicate(item, i) {
-			newArr = append(newArr, item)
+		if item == value {
+			return i
 		}
 	}
 
-	return newArr
+	return -1
 }
 
-func Some[T any](arr []T, predicate func(T, int) bool) bool {
+func Reverse[T any](arr []T) []T {
+	arrLen := len(arr)
+	n := make([]T, arrLen)
+
 	for i, item := range arr {
-		if predicate(item, i) {
-			return true
+		n[arrLen-i-1] = item
+	}
+
+	return n
+}
+
+func Remove[T comparable](arr []T, target T) []T {
+	n := []T{}
+
+	for _, item := range arr {
+		if item != target {
+			n = append(n, item)
 		}
 	}
 
-	return false
+	return n
 }
 
-func All[T any](arr []T, predicate func(T, int) bool) bool {
+func Pop[T any](arr []T, idx int) []T {
+	n := []T{}
+
 	for i, item := range arr {
-		if !predicate(item, i) {
-			return false
+		if i != idx {
+			n = append(n, item)
 		}
 	}
 
-	return true
+	return n
 }
 
-func None[T any](arr []T, predicate func(T, int) bool) bool {
-    for i, item := range arr {
-        if predicate(item, i) {
-            return false
-        }
-    }
+// Mutation Pop: mutates the given array and returns the removed item
+func MPop[T any](arr *[]T, idx int) T {
+	arrLen := len(*arr)
+	item := (*arr)[idx]
 
-    return true;
+	for i := idx; i < arrLen-1; i++ {
+		(*arr)[i] = (*arr)[i+1]
+	}
+
+	(*arr) = (*arr)[:arrLen-1]
+
+	return item
 }
-
