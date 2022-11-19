f4cb4d67f8c202f4f13d40a37247546cb753bbbf
Tch1b0 <jpour3006@gmail.com>
2022-11-19 09:17:43 +0100

    arrays: create generics file

---
+++ array/generics.go
@@ -0,0 +1,73 @@
+package array
+
+func Map[T any, N any](arr []T, predicate func(T, int) N) []N {
+	newArr := make([]N, len(arr))
+	for i, item := range arr {
+		newArr = append(newArr, predicate(item, i))
+	}
+
+	return newArr
+}
+
+func Filter[T any](arr []T, predicate func(T, int) bool) []T {
+	newArr := make([]T, len(arr))
+	for i, item := range arr {
+		if predicate(item, i) {
+			newArr = append(newArr, item)
+		}
+	}
+
+	return newArr
+}
+
+func Some[T any](arr []T, predicate func(T, int) bool) bool {
+	for i, item := range arr {
+		if predicate(item, i) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func All[T any](arr []T, predicate func(T, int) bool) bool {
+	for i, item := range arr {
+		if !predicate(item, i) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func GetFirst[T any](arr []T, predicate func(T, int) bool) *T {
+	for i, item := range arr {
+		if predicate(item, i) {
+			return &item
+		}
+	}
+
+	return nil
+}
+
+func GetAll[T any](arr []T, predicate func(T, int) bool) []T {
+	matches := []T{}
+
+	for i, item := range arr {
+		if predicate(item, i) {
+			matches = append(matches, item)
+		}
+	}
+
+	return matches
+}
+
+func None[T any](arr []T, predicate func(T, int) bool) bool {
+	for i, item := range arr {
+		if predicate(item, i) {
+			return false
+		}
+	}
+
+	return true
+}
