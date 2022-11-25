bb94d2b8b5425bbf12cc10da210647b2a13bdba6
Tch1b0 <jpour3006@gmail.com>
2022-11-25 16:24:54 +0100

    strings: enhance utility

--- strings/strings.go
+++ strings/strings.go
@@ -1,5 +1,11 @@
 package strings
 
+import (
+	"strconv"
+
+	"github.com/Tch1b0/polaris/math"
+)
+
 func Reverse(str string) string {
 	revStr := ""
 	for i := len(str) - 1; i >= 0; i-- {
@@ -7,4 +13,31 @@ func Reverse(str string) string {
 	}
 
 	return revStr
+}
+
+// returns a sorted version of the string
+func Sort(str string) string {
+	return string(math.Sort([]byte(str)))
+}
+
+// returns whether the strings are anagrams of each other
+func IsAnagram(str1, str2 string) bool {
+	return Sort(str1) == Sort(str2)
+}
+
+func Itoa(n int) string {
+	return strconv.Itoa(n)
+}
+
+func Atoi(str string) (int, error) {
+	return strconv.Atoi(str)
+}
+
+func ToRunes(str string) []rune {
+	runes := make([]rune, len(str))
+	for i, r := range str {
+		runes[i] = r
+	}
+
+	return runes
 }
