049f8a194910ce89e9abf253ad4dc8ae3cc5b2f5
Tch1b0 <Jpour3006@gmail.com>
2022-12-04 18:03:29 +0100

    math: enhance Vector2

--- math/vector2.go
+++ math/vector2.go
@@ -3,7 +3,6 @@ package math
 import (
 	"fmt"
 	"math"
-	"reflect"
 )
 
 // A vector with 2 dimensions (x, y)
@@ -12,28 +11,28 @@ type Vector2[T Number] struct {
 	Y T
 }
 
-func (v *Vector2[T]) Add(o *Vector2[T]) Vector2[T] {
+func (v Vector2[T]) Add(o Vector2[T]) Vector2[T] {
 	return Vector2[T]{
 		X: v.X + o.X,
 		Y: v.Y + o.Y,
 	}
 }
 
-func (v *Vector2[T]) Sub(o *Vector2[T]) Vector2[T] {
+func (v Vector2[T]) Sub(o Vector2[T]) Vector2[T] {
 	return Vector2[T]{
 		X: v.X - o.X,
 		Y: v.Y - o.Y,
 	}
 }
 
-func (v *Vector2[T]) Mul(o *Vector2[T]) Vector2[T] {
+func (v Vector2[T]) Mul(o Vector2[T]) Vector2[T] {
 	return Vector2[T]{
 		X: v.X * o.X,
 		Y: v.Y * o.Y,
 	}
 }
 
-func (v *Vector2[T]) ScalarMul(fac T) Vector2[T] {
+func (v Vector2[T]) ScalarMul(fac T) Vector2[T] {
 	return Vector2[T]{
 		X: v.X * fac,
 		Y: v.Y * fac,
@@ -41,24 +40,23 @@ func (v *Vector2[T]) ScalarMul(fac T) Vector2[T] {
 }
 
 // Calculate the distance between this vector and another one using the pythagora theorem
-func (v *Vector2[T]) DistanceTo(o *Vector2[T]) float64 {
+func (v Vector2[T]) DistanceTo(o Vector2[T]) float64 {
 	return math.Sqrt(math.Pow(float64(o.X-v.X), 2) + math.Pow(float64(o.Y-v.Y), 2))
 }
 
 // Calculate the distance between this vector and another one using the manhattan distance
-func (v *Vector2[T]) ManhattanDistanceTo(o *Vector2[T]) float64 {
+func (v Vector2[T]) ManhattanDistanceTo(o Vector2[T]) float64 {
 	return math.Abs(float64(v.X-o.X)) + math.Abs(float64(v.Y-o.Y))
 }
 
-func (v *Vector2[T]) String() string {
-	var x any = v.X
-	var y any = v.Y
+func (v Vector2[T]) Array() [2]T {
+	return [2]T{v.X, v.Y}
+}
 
-	numType := reflect.TypeOf(v.X).String()
+func (v Vector2[T]) String() string {
+	return fmt.Sprintf("(%v|%v)", v.X, v.Y)
+}
 
-	if numType == "float32" || numType == "float64" {
-		return fmt.Sprintf("(%f|%f)", x, y)
-	} else {
-		return fmt.Sprintf("(%d|%d)", x, y)
-	}
+func NewVector2[T Number](x T, y T) Vector2[T] {
+	return Vector2[T]{x, y}
 }
